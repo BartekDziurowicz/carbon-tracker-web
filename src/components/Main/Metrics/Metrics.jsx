9a5f74581a4d4e6a9a8c681d6b6aea8f35bf18a9
Bartosz Dziurowicz <bartosz.dziurowicz@gmail.com>
2024-05-10 19:28:46 +0200

    areas and swith

--- src/components/Main/Metrics/Metrics.jsx
+++ src/components/Main/Metrics/Metrics.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import MetricsArea from "./MetricsArea/MetricsArea.jsx";
 import MetricsStepper from "./MetricsStepper/MetricsStepper.jsx";
 import MetricsCompany from "./MetricsCompany/MetricsCompany.jsx";
 import $Metrics from "./Metrics.styles.jsx";
@@ -18,11 +19,28 @@ export default function Metrics() {
     stepHandler: stepHandler,
   };
 
+  function contentDispatcher() {
+    switch (currentStep) {
+      case 0:
+        return <MetricsCompany />;
+      case 1:
+        return <MetricsArea />;
+      case 2:
+        return <div>2 Tribes</div>;
+      case 3:
+        return <div>3 Teams</div>;
+      case 4:
+        return <div>4 Employees</div>;
+      default:
+        return <div>Something went wrong...</div>
+    }
+  }
+
   return (
     <MetricsContext.Provider value={ctxMetrics}>
       <$Metrics>
         <MetricsStepper />
-        <MetricsCompany />
+        {contentDispatcher()}
       </$Metrics>
     </MetricsContext.Provider>
   );
---
+++ src/components/Main/Metrics/MetricsArea/MetricsArea.styles.jsx
@@ -0,0 +1,11 @@
+import { styled } from "styled-components";
+
+export const $MetricsArea = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  align-content: flex-start;
+  row-gap: 20px;
+  column-gap: 20px;
+`;
