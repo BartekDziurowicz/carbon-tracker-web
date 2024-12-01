ce60aa6d9b9824c744a29f0157314f0d9ae84a91
Bartosz Dziurowicz <bartosz.dziurowicz@gmail.com>
2024-12-01 17:53:58 +0100

    home page

--- src/components/Main/Home/Home.styles.jsx
+++ src/components/Main/Home/Home.styles.jsx
@@ -1,5 +1,18 @@
 import { styled } from "styled-components";
 
 export const $Home = styled.div`
-    color: red;
+    padding: 14px 13% 0px 13%;
+`
+export const $SliderContainer = styled.div`
+    width: 75%;
+    height: auto;
+`
+
+export const $ImgContainer = styled.div`    
+    & img { 
+        width: 100%; 
+        height: auto; 
+        max-height: 650px; 
+        object-fit: cover; 
+    }
 `
