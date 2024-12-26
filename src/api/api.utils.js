f6f4ad0fd7abb0b39b7eb03440c4264c3323cfd8
Bartosz Dziurowicz <bartosz.dziurowicz@gmail.com>
2024-12-26 14:22:27 +0100

    hardcoded aws dns

--- src/api/api.utils.js
+++ src/api/api.utils.js
@@ -2,7 +2,7 @@ export const createBaseUrl = () => {
     if (window.location.host.includes('localhost')) {
         return 'http://localhost:8080';
     } else {
-        return 'api';
+        return 'http://act-alb-627176500.eu-west-1.elb.amazonaws.com/api';
     }
 };
 
